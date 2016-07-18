cbc8950c5b5eca2669b666d326d4a615782fa8f8
ericatalyst <erica825978@gmail.com>
2016-07-17 21:38:51 -0400

    Fixed editing and deleting cards

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -2,19 +2,21 @@ post '/cards/:id' do
  halt(401, "You must be logged in to a valid account in order to create a deck.") unless logged_in? && !guest_user
  card = Card.create(params[:card].merge(deck_id: params[:id]))
  @errors = card.errors.full_messages
-   redirect "/decks/#{params[:id]}"
+ redirect "/decks/#{card.deck.id}"
 end
 
 put '/cards/:id' do
   halt(401, "You did not create this deck.") unless logged_in? && !guest_user && deck_creator?
-	card = Card.create(params[:card].merge(deck_id: params[:id]))
+	card = Card.find(params[:id])
+  card.update_attributes(deck: card.deck, creator: card.creator, question: params[:question], answer: params[:answer])
   @errors = card.errors.full_messages
-  redirect "/decks/#{params[:id]}"
+  redirect "/decks/#{card.deck.id}"
 end
 
 delete '/cards/:id' do
   halt(401, "You did not create this deck.") unless logged_in? && !guest_user && deck_creator?
-	card = Card.create(params[:card].merge(deck_id: params[:id]))
+	card = Card.find(params[:id])
+  card.destroy
   @errors = card.errors.full_messages
-  redirect "/users/#{current_user.id}"
+  redirect "/decks/#{card.deck.id}"
 end
